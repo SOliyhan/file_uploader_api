f7109afa8907ce828a37832446bfd54ac7dcabe4
Shivanshu <craziest.oliyhan13@gmail.com>
2024-07-08 01:41:38 +0530

    modifying settings to allow host

--- csv_project/settings.py
+++ csv_project/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-t+v2c9c&9q#e5@2wi2d^*@vq$&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 # Application definition
